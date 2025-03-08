3b63b8357110da7e5a5389d5064c2e73d50a5faf
Temesgen Marie <tommr2323@gmail.com>
2025-03-08 21:15:03 +0300

    initial commit

---
+++ lib/crud_op.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CrudOp extends StatelessWidget {
+  CrudOp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+     return MaterialApp(
+        home: Scaffold(
+           appBar: AppBar(
+            title: Text("CRUD Operation"),
+           ),
+           body: Container(
+            child: Row(
+                
+            ),
+  
+           ),
+        ),
+     );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'crud_op.dart';
+void main() {
+  runApp( CrudOp());
+}
+ 
